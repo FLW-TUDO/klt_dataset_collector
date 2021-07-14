0e1b487587d4bc71c3736ccee58fd56e145a4712
Anas Gouda <anas.gouda@tu-dortmund.de>
2021-07-14 18:17:15 +0200

    collect data from 3 angles and save point clouds

--- scripts/klt_dataset_collector/collect_samples.py
+++ scripts/klt_dataset_collector/collect_samples.py
@@ -2,22 +2,19 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import PointCloud2
-
-import sensor_msgs.point_cloud2 as pc2
-
 from zivid_camera.srv import *
 
-import ros_numpy
+import roslaunch
+import argparse
+import os
+
 
 class Sample:
     def __init__(self):
-
         rospy.loginfo("Starting sample_capture_assistant.py")
 
         ca_suggest_settings_service = "/zivid_camera/capture_assistant/suggest_settings"
-
         rospy.wait_for_service(ca_suggest_settings_service, 29.0)
-
         self.capture_assistant_service = rospy.ServiceProxy(
             ca_suggest_settings_service, CaptureAssistantSuggestSettings
         )
@@ -26,7 +23,7 @@ class Sample:
         rospy.Subscriber("/zivid_camera/points/xyzrgba", PointCloud2, self.on_points)
 
     def capture_assistant_suggest_settings(self):
-        max_capture_time = rospy.Duration.from_sec(0.20)
+        max_capture_time = rospy.Duration.from_sec(10)
         rospy.loginfo(
             "Calling capture assistant service with max capture time = %.1f sec",
             max_capture_time.to_sec(),
@@ -43,9 +40,30 @@ class Sample:
     def on_points(self, data):
         rospy.loginfo("PointCloud received")
 
+
 def main():
+    # read arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset-path", default='/home/iiwa/segmentation/flw_dataset')
+    parser.add_argument("--start-count", type=int, default=0)
+    args = parser.parse_args()
+
+    if not os.path.exists(args.dataset_path):
+        rospy.logerr("Error: dataset folder doesn't exist")
+        exit()
+
     rospy.init_node('data_collector', anonymous=True)
     pub = rospy.Publisher('/iiwa/command/CartesianPose', PoseStamped, queue_size=1)
+
+    # setup PCD cloud saver
+    saver_package = 'pcl_ros'
+    saver_node = 'pointcloud_to_pcd'
+    point_cloud_topic = '/passthrough/box_filtered'
+    frame = 'bin_link'
+    cli_args = ['input:=' + point_cloud_topic,
+                '_binary:=True',
+                '_fixed_frame:=' + frame,
+                '_prefix:=' + args.dataset_path]
 
     while pub.get_num_connections() == 0:
         rospy.sleep(0.1)
@@ -69,7 +87,25 @@ def main():
     msg = PoseStamped()
     msg.header.stamp = rospy.Time.now()
     msg.header.frame_id = 'iiwa_link_0'
+
+    count = args.start_count
+    sample_dir = args.dataset_path + '/' + f"{count:05}"
+    if os.path.exists(sample_dir):
+        rospy.logerr("Sample number already exist. Count: " + str(count))
+        rospy.logerr("Need manual help, Exiting ...")
+        exit()
+    else:
+        os.mkdir(sample_dir)
+
     for i in range(len(position)):
+        # start cloud saver
+        launch = roslaunch.scriptapi.ROSLaunch()
+        launch.start()
+        cli_args[3] = '_prefix:=' + sample_dir + '/cloud_' + str(i) + '_'
+        node = roslaunch.core.Node(saver_package, saver_node, args=' '.join(cli_args))
+        process = launch.launch(node)
+
+        # format Pose msg
         msg.header.seq = i
         msg.pose.position.x = position[i][0]
         msg.pose.position.y = position[i][1]
@@ -79,20 +115,14 @@ def main():
         msg.pose.orientation.z = orientation[i][2]
         msg.pose.orientation.w = orientation[i][3]
         pub.publish(msg)
-        rospy.sleep(10) # wait till robot reach goal position
-        s.capture_assistant_suggest_settings()
+        rospy.sleep(10)  # wait till robot reach goal position
+        s.capture_assistant_suggest_settings()  # TODO is capture assistant required after every frame?
         rospy.sleep(3)
         s.capture()
         pc2_msg = rospy.wait_for_message("/zivid_camera/points/xyzrgba", PointCloud2)
 
-        gen = pc2.read_points(pc2_msg, skip_nans=True, field_names=("x", "y", "z"))
-        print(gen)
-
-        #for p in pc2.read_points(pc2_msg, field_names = ("x", "y", "z", "rgba"), skip_nans=True):
-        #    print(" x : %f  y: %f  z: %f rgba: %f" %(p[0],p[1],p[2],p[3]))
-
-        rospy.sleep(1) # wait camera capture frame
-
+        rospy.sleep(2)  # wait camera capture frame and point cloud saver saves it
+        process.stop()
 
 if __name__ == '__main__':
     try:
